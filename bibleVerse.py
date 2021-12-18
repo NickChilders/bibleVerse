4d7286123c8f35edc629d33cc79c1c5419227465
nrchilders <74997040+nrchilders@users.noreply.github.com>
2021-12-18 16:36:07 -0500

    GUI
    Updates the src code to include a GUI. Functional, however very bare bones right now.

--- bibleVerse.py
+++ bibleVerse.py
@@ -1,133 +1,120 @@
-import os
 from textwrap import wrap
 
-#--This function clears the screen--#
-def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
+from tkinter import *
 
-cls()
+top = Tk()
 
-#--This just prints a formatted border around text. Strictly cosmetic.--#
-sentence1 = "Welcome to Bible Verse!\t"
-width = 100
-print('+-' + '-' * width + '-+')
-for line in wrap(sentence1, width):
-    print('| {0:^{1}} |'.format(line, width))
-print('+-' + '-'*(width) + '-+')
-print()
+#getting screen width and height of display
+width= top.winfo_screenwidth() 
+height= top.winfo_screenheight()
+#setting tkinter window size
+top.geometry("%dx%d" % (width, height))
+top.title("BibleVerse")
 
-#--This function takes in the file to open. Then searches for the requested verses and prints them out.--# 
-def play(file):
-    #--Condition flags--#
-    #Verse was found
-    foundFlag = 0
-    #Stop reading lines from file
-    stopFlag = 0
-    #User requested multiple verses
-    multiFlag = 0
-    #Line number
-    index = 0
+LF = LabelFrame(top, text = "Welcome to BibleVerse!", font ="Courier 18 bold", labelanchor = N)
+LF.pack(fill = "both", expand = "yes")
 
-    #--Grab desired input from user and save to variables--#
-    print("Please enter the following information: ")
-    book = input("Book: ")
-    chapter = input("Chapter: ")
-    verse = input("Verse: ")
-    userInput = book + " " + chapter + ":" + verse
-    
-    cls()
+multiStr = ""
 
-    #--This just prints a formatted border around text. Strictly cosmetic.--#
-    sentence2 = userInput
-    print('+-' + '-' * width + '-+')
-    for line in wrap(sentence2, width):
-        print('| {0:^{1}} |'.format(" ", width))
-        print('| {0:^{1}} |'.format(line, width))
-        print('| {0:^{1}} |'.format(" ", width))
-    print('+-' + '-'*(width) + '-+')
-    print() 
-    
-    bible = open(file, "r")
-    
-    #If the user wants multiple verses
-    if "-" in verse:
-        lhs, rhs = verse.split("-", 1)
-        lhs = book + " " + chapter + ":" + lhs + "\t"
-        rhs = book + " " + chapter + ":" + rhs + "\t"
-        multiFlag = 1
-    
-    #If user only wants a single verse
-    if multiFlag == 0:
+#-------------------------------------------------------------------------#
+def displayText(line):
+  
+  label.configure(text=line)
+  label.place(x = (width/2)-400, y = 400)
+  
+  B2 = Button(top, text="Remove Text", command= clearText)
+  B2.place(x = (width/2)-150, y = 250)
+#-------------------------------------------------------------------------#
+
+def concatLine(line):
+  global multiStr
+  newStr = multiStr + line + "\n"
+  multiStr = newStr
+#-------------------------------------------------------------------------#
+def clearText():
+  global multiStr
+  label.configure(text = "")
+  multiStr = ""
+#-------------------------------------------------------------------------#
+
+#-------------------------------------------------------------------------#
+def play():
+  file = "Bible.txt"
+  book = E1.get()
+  chapter = E2.get()
+  verse = E3.get()
+
+  #--Condition flags--#
+  #Verse was found
+  foundFlag = 0
+  #Stop reading lines from file
+  stopFlag = 0
+  #User requested multiple verses
+  multiFlag = 0
+  #Line number
+  index = 0
+  #Concatenate entries
+  userInput = book + " " + chapter + ":" + verse
+
+  bible = open(file, "r")
+
+  #If the user wants multiple verses
+  if "-" in verse:
+    lhs, rhs = verse.split("-", 1)
+    lhs = book + " " + chapter + ":" + lhs
+    rhs = book + " " + chapter + ":" + rhs
+    multiFlag = 1
+  #If user only wants a single verse
+  if multiFlag == 0:
+    for line in bible:
+      index += 1
+      if userInput in line:
+        foundFlag = 1
+        break
+    if foundFlag == 1:
+      displayText(line)
+
+  #If user wants multiple verses
+  else:
+    stopFlag = 1
+    for line in bible:
+      if stopFlag == 0:
+        displayText(multiStr)
+        break
+      index += 1
+      if lhs in line:
+        foundFlag = 1
+        concatLine(line)
         for line in bible:
-            index += 1
-            if userInput in line:
-                foundFlag = 1
-                break
-        if foundFlag == 1:
-            print(line)
-        else:
-            #--This prints a formatted border around an error message.--#
-            error = userInput + " Does Not Exist!"
-            print('+-' + '-' * width + '-+')
-            for line in wrap(error, width):
-                print('| {0:^{1}} |'.format(" ", width))
-                print('| {0:^{1}} |'.format(line, width))
-                print('| {0:^{1}} |'.format("Please check spelling, chapter number, and verse number", width))
-                print('| {0:^{1}} |'.format(" ", width))
-            print('+-' + '-'*(width) + '-+')
-            print()
-    
-    #If user wants multiple verses
-    else:
-        stopFlag = 1
-        for line in bible:
-            if stopFlag == 0:
-                break
-            index += 1
-            if lhs in line:
-                foundFlag = 1
-                print(line)
-                for line in bible:
-                    index += 1
-                    print(line)
-                    if rhs in line:
-                        stopFlag = 0
-                        break
-            else:
-                #--This prints a formatted border around an error message.--#
-                multiError = userInput + " Do Not Exist!"
-                print('+-' + '-' * width + '-+')
-                for line in wrap(multiError, width):
-                    print('| {0:^{1}} |'.format(" ", width))
-                    print('| {0:^{1}} |'.format(line, width))
-                    print('| {0:^{1}} |'.format("Please check spelling, chapter number, and verse numbers", width))
-                    print('| {0:^{1}} |'.format(" ", width))
-                print('+-' + '-'*(width) + '-+')
-                print()
-                stopFlag = 0
-    #--Check to see if the user would like to keep running the program.--#
-    again = input("Would you like another verse? (y/n) ")
-    #Return 0 if no
-    if again != 'y':
-        bible.close()
-        return 0
-    #Return 1 if yes    
-    else:
-        bible.close()
-        return 1
-        
-keepPlaying = 1
-#Keep running if the user wants to keep going.
-while keepPlaying == 1:
-    keepPlaying = play("Bible.txt")
+          index += 1
+          concatLine(line)
+          if rhs in line:
+            stopFlag = 0
+            break
+#------------------------------------------------------------#
 
-cls()
-#--This just prints a formatted border around text. Strictly cosmetic.--#
-sentence3 = "Thank you for using Bible Verse!\t"
-print('+-' + '-' * width + '-+')
-for line in wrap(sentence3, width):
-    print('| {0:^{1}} |'.format(" ", width))
-    print('| {0:^{1}} |'.format(line, width))
-    print('| {0:^{1}} |'.format(" ", width))
-print('+-' + '-'*(width) + '-+')
-print()    
+#------------------------------------------------------------#
+L1 = Label(top, text = "Enter a Book: ", font="Courier")
+L1.place(x = (width/2)-200, y = 100)
+E1 = Entry(top, bd = 5)
+E1.place(x =  (width/2)+25 , y = 100)
+
+
+L2 = Label(top, text = "Enter a Chapter: ", font="Courier")
+L2.place(x = (width/2)-200, y = 150)
+E2 = Entry(top, bd = 5)
+E2.place(x =  (width/2)+25, y = 150)
+
+
+L3 = Label(top, text = "Enter a Verse: ", font="Courier")
+L3.place(x = (width/2)-200, y = 200)
+E3 = Entry(top, bd = 5)
+E3.place(x =  (width/2)+25, y = 200)
+
+
+B1 = Button(top, text="Find", command= play)
+B1.place(x =  (width/2)+25, y = 250)
+
+label = Label(top, text="", font = ("Courier 12"), wraplength=1000, justify= LEFT)
+
+top.mainloop()
